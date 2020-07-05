7d77870efa5f0b02a3ffe3418dfae0109535cd6c
huyphung1602 <huy.phung.sw@gmail.com>
2020-07-06 02:51:06 +0700

    Add relation folder, add basic column type check method

---
+++ connection_adapter/column.rb
@@ -0,0 +1,14 @@
+class Column
+  TYPES_MAP = {
+    'integer': 'integer',
+    'boolean': 'boolean',
+    'character varying(255)': 'string',
+    'character varying': 'string'
+    'timestamp without time zone': 'datetime',
+    'jsonb': 'jsonb',
+  }
+
+  def self.get_column_type(type)
+    TYPES_MAP[type]
+  end
+end
---
+++ relation/where_clause.rb
@@ -0,0 +1,2 @@
+class WhereClause
+end
