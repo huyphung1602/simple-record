f672c4faa9c20a7ffaff34652b75777aa37c31d4
huyphung1602 <huy.phung.sw@gmail.com>
2020-07-06 10:51:15 +0700

    Turn off debug logs in simple_cache

--- simple_cache.rb
+++ simple_cache.rb
@@ -4,12 +4,12 @@ class SimpleCache
   def self.fetch key, &block
     if @redis.key?(key)
       # fetch and return result
-      puts "fetch from cache"
+      # p "fetch from cache"
       @redis[key]
     else
       if block_given?
         # make the DB query and create a new entry for the request result
-        puts "did not find key in cache, executing block ..."
+        # p "did not find key in cache, executing block ..."
         @redis[key] = yield(block)
       end
     end
