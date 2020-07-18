5ea46a3b2d6d5cb5ca1e15959208975b85432ead
huyphung1602 <huy.phung.sw@gmail.com>
2020-07-18 22:34:11 +0700

    Complete restructure, now we can use lazy load with include

--- relation.rb
+++ relation.rb
@@ -8,7 +8,7 @@ class Relation
     @table_name = table_name
     @col_definitions = col_definitions
     @primary_key = primary_key
-    @sub_clause = nil
+    @includes_params = []
     @association_cache = {}
     @select_clause = nil
     @where_clause = nil
@@ -35,6 +35,16 @@ class Relation
     self
   end
 
+  def includes(*args)
+    args.each do |association_name|
+      reflection = SchemaCache.fetch "#{@table_name}_reflections"
+      foreign_key = reflection.reflections[association_name][:foreign_key]
+      @includes_params << [association_name.to_s.classify.constantize, foreign_key]
+    end
+
+    self
+  end
+
   def to_sql
     sql = <<~SQL
       #{@select_clause.value}
@@ -47,11 +57,23 @@ class Relation
     column_names = args.map(&:to_s)
     @select_clause.update(column_names)
 
-    if @association_cache[@table_name.to_sym]
+    result = if @association_cache[@table_name.to_sym]
       @association_cache[@table_name.to_sym]
     else
       @table_name.classify.constantize.evaluate_relation(self.to_sql, column_names)
     end
+
+    if @includes_params.any?
+      primary_keys = result.map { |r| r.send(@primary_key) }
+
+      @includes_params.each do |params|
+        association_class, foreign_key = params
+        includes_query_result = association_class.where("#{foreign_key}": primary_keys).evaluate
+        includes_result_distribution(includes_query_result, association_class, foreign_key)
+      end
+    end
+
+    result
   end
 
   def set_association_cache(association_cache)
@@ -74,6 +96,25 @@ class Relation
       self.evaluate.send(method)
     else
       super
+    end
+  end
+
+  def includes_result_distribution(includes_query_result, association_class, foreign_key)
+    hash = {}
+    includes_query_result.each do |r|
+      sql = association_class.where("#{foreign_key}": r.send(foreign_key)).to_sql
+
+      if hash[sql].nil?
+        hash[sql] = []
+      else
+        hash[sql] << r
+      end
+    end
+
+    hash.each do |k, v|
+      QueryCache.fetch k do
+        v
+      end
     end
   end
 end
